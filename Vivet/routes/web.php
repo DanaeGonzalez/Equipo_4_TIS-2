19d8804dd216633858175607b5c613c9113784b0
Javier23x <112022131+Javier23x@users.noreply.github.com>
2025-04-18 13:27:35 -0400

    Plantilla landing
    Esta mas o menos cargada aun le falta corregir cosas como las rutas y eso pero estamos en eso.

--- Vivet/routes/web.php
+++ Vivet/routes/web.php
@@ -1,18 +1,54 @@
+// routes/web.php
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
+// Página principal (index.html -> welcome.blade.php)
 Route::get('/', function () {
-    return view('landing');
+    return view('welcome');
+})->name('home');
+
+// Páginas estáticas
+Route::prefix('pages')->group(function () {
+    Route::get('/about', function () {
+        return view('pages.about');
+    })->name('about');
+
+    Route::get('/contact', function () {
+        return view('pages.contact');
+    })->name('contact');
+
+    Route::get('/pricing', function () {
+        return view('pages.pricing');
+    })->name('pricing');
+});
+
+// Blog
+Route::prefix('blog')->group(function () {
+    Route::get('/grids', function () {
+        return view('blog.grids');
+    })->name('blog.grids');
+
+    Route::get('/details/{id}', function ($id) {
+        return view('blog.details', ['id' => $id]);
+    })->name('blog.details');
+});
+
+// Autenticación
+Route::prefix('auth')->group(function () {
+    Route::get('/signin', function () {
+        return view('auth.signin');
+    })->name('auth.signin');
+
+    Route::get('/signup', function () {
+        return view('auth.signup');
+    })->name('auth.signup');
+});
+
+// Manejo de errores (404.html)
+Route::fallback(function () {
+    return view('errors.404');
 });
