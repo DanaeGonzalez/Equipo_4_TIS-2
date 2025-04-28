1014fcccca33e1d3219ba4f587e1db42e8388575
Javier23x <112022131+Javier23x@users.noreply.github.com>
2025-04-27 20:08:14 -0400

    Creacion carpeta de rutas

---
+++ Vivet/routes/billing.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BillingController;
+
+// Facturación y caja
+Route::middleware(['auth'])->group(function () {
+    Route::resource('billing', BillingController::class);
+});
