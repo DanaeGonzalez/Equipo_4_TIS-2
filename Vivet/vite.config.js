43f502cf1164974e426438566578c3c29839f8fc
Javier23x <112022131+Javier23x@users.noreply.github.com>
2025-05-01 12:54:18 -0400

    Landing

--- Vivet/vite.config.js
+++ Vivet/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from 'tailwindcss'; // Importa Tailwind
 
+import flowbite from 'flowbite/plugin';
+
 export default defineConfig({
   plugins: [
+    [flowbite],
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
       refresh: true,
