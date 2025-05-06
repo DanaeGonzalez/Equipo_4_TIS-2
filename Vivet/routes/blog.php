d8bdda0972e007e80123fc4f1fba2cf130bc3402
DanaeGonzalez <danaegv2002@gmail.com>
2025-05-06 01:18:43 -0400

    Update blog.php
    Se comenta para doble seguridad PostController

--- Vivet/routes/blog.php
+++ Vivet/routes/blog.php
@@ -1,11 +1,11 @@
 <?php
-
+/*
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+//use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostCategoryController;
 
 // Blog y noticias
 Route::middleware(['auth'])->group(function () {
     Route::resource('posts', PostController::class);
     Route::resource('post-categories', PostCategoryController::class);
-});
+});*/
