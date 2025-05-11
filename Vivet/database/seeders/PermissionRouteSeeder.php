cfe4f40bb42c3a3d3472390ea7995aa6c4511627
DanaeGonzalez <danaegv2002@gmail.com>
2025-05-11 19:47:22 -0400

    Update Permissions
    Ahora se pueden crear y actualizar sin problemas los productos, servicios, roles, etc

--- Vivet/database/seeders/PermissionRouteSeeder.php
+++ Vivet/database/seeders/PermissionRouteSeeder.php
@@ -11,19 +11,25 @@ class PermissionRouteSeeder extends Seeder
     {
         $map = [
             'Ver Productos' => 'products.index',
-            'Crear Productos' => 'products.create',
-            'Editar Productos' => 'products.edit',
-            'Eliminar Productos' => 'products.destroy',
+            'Crear Producto' => 'products.create',
+            'Guardar Producto' => 'products.store',
+            'Editar Producto' => 'products.edit',
+            'Eliminar Producto' => 'products.destroy',
+            'Actualizar Producto' => 'products.update',
 
             'Ver Servicios' => 'services.index',
             'Crear Servicios' => 'services.create',
+            'Guardar Servicios' => 'services.store',
             'Editar Servicios' => 'services.edit',
             'Eliminar Servicios' => 'services.destroy',
+            'Actualizar Servicios' => 'services.update',
 
             'Ver Citas' => 'schedules.index',
             'Crear Citas' => 'schedules.create',
+            'Guardar Citas' => 'schedules.store',
             'Editar Citas' => 'schedules.edit',
             'Eliminar Citas' => 'schedules.destroy',
+            'Actualizar Citas' => 'schedules.update',
 
         ];
 
