71aabd360937c68905a709266f20f7eb99cd53ce
Javier23x <112022131+Javier23x@users.noreply.github.com>
2025-05-27 22:58:02 -0400

    Todo borrado
    Para volver atras si pasa algo

--- Vivet/config/app.php
+++ Vivet/config/app.php
@@ -195,7 +195,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Stancl\Tenancy\TenancyServiceProvider::class,
-        App\Providers\AdminPanelProvider::class,
 
     ],
 
