4393db5819c6d5763885621a74874ec136add2ef
Natter21 <142517692+Natter21@users.noreply.github.com>
2025-05-27 22:16:23 -0400

    dad

--- Vivet/routes/notes.php
+++ Vivet/routes/notes.php
@@ -11,8 +11,6 @@ Route::middleware(['check.permission'])->group(function () {
     Route::get('/notes/{note}/edit', [NoteController::class, 'edit'])->name('notes.edit');
     Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
     Route::delete('/notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
-    
-
 });
 
 
