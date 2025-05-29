a081aa0be282ab188935d6694181de96687477e7
R3kiem <cavendano@ing.ucsc.cl>
2025-05-28 21:52:05 -0400

    Update header.blade.php
    - Agregada la ruta hacia los exámenes en el panel de admin.

--- Vivet/resources/views/tenant/partials/header.blade.php
+++ Vivet/resources/views/tenant/partials/header.blade.php
@@ -126,6 +126,15 @@
                   </a>
                 </li>
                 <li>
+                  <a href="{{ route('exams.index') }}" class="flex items-center p-2 text-brown-800 rounded-lg hover:bg-blue-100 group">
+                    <!-- Schedules/Clock Icon -->
+                    <svg class="flex-shrink-0 w-5 h-5 text-blue-700 transition duration-75 group-hover:text-blue-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
+                    </svg>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Examenes</span>
+                  </a>
+                </li>
+                <li>
                   <a href="{{ route('schedules.index') }}" class="flex items-center p-2 text-brown-800 rounded-lg hover:bg-blue-100 group">
                     <!-- Schedules/Clock Icon -->
                     <svg class="flex-shrink-0 w-5 h-5 text-blue-700 transition duration-75 group-hover:text-blue-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
