a55a0d370613ca74989bb60784102992615026fe
Danae Eilyn González Valdés <danaegv2002@gmail.com>
2025-06-12 22:20:04 -0400

    Update Seeders
    se actualizan carpetas de los seeders

--- Vivet/database/seeders/DatabaseSeeder.php
+++ Vivet/database/seeders/DatabaseSeeder.php
@@ -9,8 +9,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            SuperAdminSeeder::class,
-            //TenantSeeder::class,
+            RolesTableSeeder::class,
+            PermissionsTableSeeder::class,
+            PermissionRouteSeeder::class,
+            //SuperAdminSeeder::class,
+            TutorSeeder::class,
+            VeterinarioSeeder::class,
+            AdminSeeder::class,
+            ServicesTableSeeder::class,
+            VeterinarioSeeder::class,
         ]);
         
     }
