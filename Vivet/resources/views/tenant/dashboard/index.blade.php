97c5d2616d4973ede1ac21133e58149723bcae8c
Javier23x <112022131+Javier23x@users.noreply.github.com>
2025-06-15 00:50:38 -0400

    Componente de tabla

--- Vivet/resources/views/tenant/dashboard/index.blade.php
+++ Vivet/resources/views/tenant/dashboard/index.blade.php
@@ -4,4 +4,33 @@
 
 @section('content')
 
+<section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
+    <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
+
+        <div class="flex flex-col lg:flex-row items-start md:items-center justify-between gap-2 mb-4">
+            {{-- Título a la izquierda --}}
+            <h1 class="text-xl font-semibold text-gray-800 dark:text-white">
+                Tabla de Ejemplo
+            </h1>
+
+            {{-- Breadcrumb a la derecha --}}
+            @include('tenant.partials.dashboard.breadcrumbs', [
+            'items' => [
+            ['name' => 'Ayuda', 'url' => '#'],
+            ['name' => 'Ejemplo'],
+            ]
+            ])
+        </div>
+
+        <x-dashboard.table
+            :headers="['Producto', 'Color', 'Categoría', 'Precio']"
+            :rows="[
+        ['columns' => ['Surface Pro', 'White', 'PC', '$1999']],
+        ['columns' => ['iPad', 'Gold', 'Tablet', '$699']],
+    ]" />
+
+
+
+    </div>
+</section>
 @endsection
