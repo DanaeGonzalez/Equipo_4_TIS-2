03b7a16126e93084b8b6af4bd0a9aaac3d5feac0
Javier23x <112022131+Javier23x@users.noreply.github.com>
2025-06-14 22:16:32 -0400

    Breadcrumbs

--- Vivet/resources/views/tenant/dashboard/modules/example/index.blade.php
+++ Vivet/resources/views/tenant/dashboard/modules/example/index.blade.php
@@ -1,5 +1,14 @@
 @extends('tenant.layouts.dashboard')
 @section('content')
+
+<!-- Breadcrumbs -->
+@include('tenant.partials.dashboard.breadcrumbs', [
+    'items' => [
+        ['name' => 'Ayuda', 'url' => '#'],
+        ['name' => 'Ejemplo'],
+    ]
+])
+
 <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
     <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
         <!-- Start coding here -->
